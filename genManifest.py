629966fb0d93466afd14e03867a34271faeae2fa
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-14 16:04:18 +0200

    remove entrys before serialisation

--- genManifest.py
+++ genManifest.py
@@ -53,9 +53,8 @@ def convertJSON(infile, outfile):
                 else:
                     part['size'] = None  # If the file doesn't exist, set size to None
 
+        clean_json(data)
         j = json.dumps(data, indent=4)
-        print("json:", j)
-        clean_json(j)
         print("entry removed json:", j)
         # Write updated data to JSON
         with open(outfile, "w") as f:
